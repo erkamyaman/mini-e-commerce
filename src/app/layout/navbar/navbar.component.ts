0c799d5fd0dc614affaa8a95a1d14a0eb4c2e5a0
Kam <erkamyaman35@gmail.com>
2025-05-11 20:24:43 +0300

    add login, orders, products, sales, and signup components with basic templates and styles

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -3,10 +3,11 @@ import { Toolbar } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
 import { SharedModule } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [Toolbar, AvatarModule, ButtonModule],
-  templateUrl: './navbar.component.html',
+  imports: [Toolbar, AvatarModule, ButtonModule, RouterLink],
+  templateUrl: './navbar.component.html'
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
