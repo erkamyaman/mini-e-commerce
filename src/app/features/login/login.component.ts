b57c5f8925599505b6c0c13a038833a3eea0f635
Kam <erkamyaman35@gmail.com>
2025-05-11 23:09:48 +0300

    add login component with styles and reactive form setup

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(public fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
+  }
 }
