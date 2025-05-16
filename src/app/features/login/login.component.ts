fa9c5a54f09e5fd93f5169ad2747013877fe80a4
Kam <erkamyaman35@gmail.com>
2025-05-16 15:36:45 +0300

    feat: add json-server and json-server-auth for mock API support
    style: update login component styles for better spacing and alignment
    
    fix: enhance login form with validation and error messages
    
    feat: create product card component with basic structure
    
    chore: add auth service for handling authentication logic

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,21 +1,37 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, InputTextModule, ButtonModule],
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
+  submitted = false;
 
   constructor(public fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      username: new FormControl(''),
-      password: new FormControl('')
-    })
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    }, { updateOn: 'submit' })
+  }
+
+  submitLogin() {
+    this.submitted = true
+    console.log('login')
+  }
+
+  invalidFieldChecker(fieldName: string) {
+    const control = this.loginForm.get(fieldName);
+    return this.submitted && control?.invalid;
+  }
+
+  invalidFieldMessage(fieldName: string): string {
+    return `Where is the ${fieldName}???`;
   }
 }
